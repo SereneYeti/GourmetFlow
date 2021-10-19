1470416740b525dc96ad94a9c7d842d33a1d514d
James D Vieyra <james@vieyra.co.za>
2021-10-19 12:41:53 +0200

    Initial commit

---
+++ app/src/main/java/com/vieyra18022490/gourmetflow/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vieyra18022490.gourmetflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
