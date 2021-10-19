beec16800d81a64e2d59f15124c0a3128798af9d
James D Vieyra <james@vieyra.co.za>
2021-10-19 12:48:11 +0200

    Update MainActivity.java
    ugdiuehf

--- app/src/main/java/com/vieyra18022490/gourmetflow/MainActivity.java
+++ app/src/main/java/com/vieyra18022490/gourmetflow/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //Can you see this.
+    //new change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
